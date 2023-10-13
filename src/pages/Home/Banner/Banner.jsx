fccf582bc65747eceb1dcd41b0622b16835bd988
Prosantaroy1 <royprosanta172@gmail.com>
2023-10-14 00:19:11 +0600

    banner img add

---
+++ src/pages/Home/Banner/Banner.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Banner = () => {
+    return (
+        <div className='mb-12'>
+            <img src="../../../../public/banner-and-eCommerce.jpg" className='h-96 w-full' alt="" />
+        </div>
+    );
+};
+
+export default Banner;
--- src/pages/Shared/Navber/Navber.jsx
+++ src/pages/Shared/Navber/Navber.jsx
@@ -4,10 +4,10 @@ import { Link } from 'react-router-dom';
 const Navber = () => {
 
     const navlist=<>
-        <li className='text-xl'>Home</li>
-        <li className='text-xl'>Product</li>
-        <li className='text-xl'>Service</li>
-        <li className='text-xl'>Contact</li>
+        <li className='text-lg'><Link className='hover:text-yellow-300' to='/'>Home</Link></li>
+        <li className='text-lg'><Link className='hover:text-yellow-300'>Product</Link></li>
+        <li className='text-lg'><Link className='hover:text-yellow-300'>Service</Link></li>
+        <li className='text-lg'><Link className='hover:text-yellow-300'>Contact</Link></li>
     </>
 
     return (
@@ -18,14 +18,14 @@ const Navber = () => {
       <label tabIndex={0} className="btn btn-ghost lg:hidden">
         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
       </label>
-      <ul tabIndex={0} className="menu menu-sm dropdown-content gap-5 mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+      <ul tabIndex={0} className="menu menu-sm dropdown-content  gap-4 mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
         {navlist}
       </ul>
     </div>
     <a className="btn btn-ghost normal-case text-2xl">T-SHOP</a>
   </div>
   <div className="navbar-center hidden lg:flex">
-    <ul className="menu menu-horizontal px-1 gap-5">
+    <ul className="flex flex-row px-1 gap-6">
       {navlist}
     </ul>
   </div>
